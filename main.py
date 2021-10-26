71c03351fd8205ff3b1b48e37dbf15b3d1a41991
João Faria <joao.miguel.correia.faria@tecnico.ulisboa.pt>
2021-10-26 19:04:11 +0100

    Ideias principais

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
+from z3 import *
+
+def sudoku(P):
+    s = Solver()
+    s.add(...)
+    s.check()
+
+    return unsat if s
+
 def well_posed(P):
-    return sudoku(P) and sudoku(P and not sudoku(P))
+    sol = sudoku(P)
+    tem_sol = sol != unsat
+    return sol != unsat and sudoku(P and not sudoku(P))
