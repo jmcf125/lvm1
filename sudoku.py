041488e9f311c303a5fb3c1a6bc1349c6d53b6a2
João Miguel <jmcf125 at openmailbox dot org>
2021-10-30 19:12:45 +0100

    Adicionar variantes do sudoku

--- sudoku.py
+++ sudoku.py
@@ -1,4 +1,3 @@
-# -*- coding: future_fstrings -*-
 from z3 import *
 
 P_expl = [[5,3,0,0,7,0,0,0,0],
@@ -11,25 +10,29 @@ P_expl = [[5,3,0,0,7,0,0,0,0],
           [0,0,0,4,1,9,0,0,5],
           [0,0,0,0,8,0,0,7,9]]
 
-def sudoku(P):
+def restrs_usuais(P):
     s = Solver()
     l = len(P)
-    #   Inicializa matriz de arrays (9x9 matriz com cada entry sendo um array de 9 numeros)
-    Prop = [[[Bool(f'p_{i}_{j}_{k}') for k in range(l)] for j in range(l)] for i in range(l)]
-    #print(Prop)
+    #   Inicializa matriz de arrays (9x9 matriz com cada entry sendo um array
+    #   de 9 números):
+    Prop = [[[Bool(f'p_{i}_{j}_{k}') for k in range(l)]
+                                     for j in range(l)]
+                                     for i in range(l)]
+
     for i in range(l):
         for j in range(l):
             if(P[i][j]!=0):
                 s.add(Prop[i][j][ P[i][j]-1 ])
-
-    #print(s)
 
     # === Conditions === #
 
     # -- Pelo menos um numero em cada célula
     for i in range(l):
         for j in range(l):
-            s.add(Or(Prop[i][j]))   #Prop[i][j] = [p_i_j_1, p_i_j_2, ..., p_i_j_9]  <--> p_i_j_1 V p_i_j_2 V ...
+            s.add(Or(Prop[i][j]))
+            #Prop[i][j] = [p_i_j_1, p_i_j_2, ..., p_i_j_9]
+            #                      <-->
+            #              p_i_j_1 V p_i_j_2 V ...
 
     # -- No maximo um numero em cada celula
     for i in range(l):
@@ -38,7 +41,10 @@ def sudoku(P):
                 for m in range(k+1, l):
                     s.add(Or( Not(Prop[i][j][k]), Not(Prop[i][j][m]) ))
 
+    return (s, l, Prop)
 
+def sudoku(P):
+    (s, l, Prop) = restrs_usuais(P)
     #if(s.check()==sat):
     #    #print(s.model())
     #    print_board(s.model(), P)
@@ -47,6 +53,62 @@ def sudoku(P):
 
     return unsat if check == unsat else s.model()
 
+P_sudoku_var_expl = [4,0,0, 7,0,6, 0,0,9,
+                     0,0,6, 1,5,9, 4,0,0,
+                     1,5,0, 0,8,0, 0,2,6,
+
+                     0,0,7, 0,6,0, 5,0,0,
+                     2,0,1, 0,0,0, 8,0,7,
+                     0,0,4, 0,3,0, 2,0,0,
+
+                     3,7,0, 0,1,0, 0,4,8,
+                     0,0,5, 9,4,8, 3,0,0,
+                     9,0,0, 3,0,2, 0,0,5]
+
+def sudoku_var(P, variantes):
+    (s, l, Prop) = restrs_usuais(P)
+
+    # Função que garante que as células a deltas de distância específicos não
+    # têm deltas de valor prescritos. Por expl., p/ deltas_vals = [0], garante
+    # que as células são diferentes.
+    def diferentes(deltas_dist, deltas_vals):
+        for i in range(l):
+            for j in range(l):
+                for n in range(1,l+1):
+                    # ps -- possibilidades de (i,j) + (delta-i, delta-j)
+                    ps = [(i+di, j+dj) for (di,dj) in deltas
+                                        if 8 >= i + di >= 0 and
+                                           8 >= j + dj >= 0]
+
+                    # Se a célula (i,j) vale n, as que estão a delta de
+                    # distância não podem valer o mesmo:
+                    s.add(And([Implies(
+                        Prop[i][j][n-1],
+                        Not[Prop[p[0]][p[1]][m-1]])
+                            for p in ps
+                            for m in [n + dn for dn in deltas_vals]
+                                if 1 <= m <= 9]))
+
+    # Nota: nestas duas primeiras variantes, podemos optimizar as restrições
+    #       -- por expl. se a != b, então b!= a -- contudo, estas optimizações
+    #       ainda não estão implementadas.
+
+    # Células a passo-de-cavalo têm de ser diferentes
+    if 1 in variantes:
+        diferentes([(1,  2), ( 1,-2), ( 2, 1), ( 2,-1),
+                    (-1,-2), (-1, 2), (-2,-1), (-2, 1)]
+                  ,[0])
+
+    # Células a passo-de-rei têm de ser diferentes
+    elif 2 in variantes:
+        diferentes([(-1,  0), ( 0, 1), ( 1,-1), ( 1, 1),
+                    ( 1,  0), ( 0,-1), (-1, 1), (-1,-1)]
+                  ,[0])
+
+    # Células ortogonalmente adjacentes não podem ter números adjacentes
+    elif 3 in variantes:
+        diferentes([(-1,0), (0,1), (1,0), (-1,0)]
+                  ,[-1,+1])
 
 def well_posed(P):
     sol = sudoku(P)
